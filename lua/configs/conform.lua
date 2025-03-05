c30d20af9e45359870b1d631335529320f931ba0
Greg O'Grady <gregory.ogrady@voiceflow.com>
2025-03-05 17:20:17 -0500

    feat: stylua formatting

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,21 +1,31 @@
 local options = {
-	formatters_by_ft = {
-		lua = { "stylua" },
-		javascript = { "prettier" },
-		sql = { "sqlfmt" },
-		json = { "prettier" },
-		typescript = { "prettier" },
-		sh = { "shfmt" },
-		yaml = { "prettier" },
-		-- css = { "prettier" },
-		-- html = { "prettier" },
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+    javascript = { "prettier" },
+    sql = { "sqlfmt" },
+    json = { "prettier" },
+    typescript = { "prettier" },
+    sh = { "shfmt" },
+    yaml = { "prettier" },
+    -- css = { "prettier" },
+    -- html = { "prettier" },
+  },
 
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+
+  formatters = {
+    stylua = {
+      options = {
+        indent_type = "Spaces",
+        indent_width = 2,
+        call_parentheses = "Always",
+      },
+    },
+  },
 }
 
 return options
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,114 +1,114 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = "BufWritePre", -- uncomment for format on save
-		opts = require("configs.conform"),
-	},
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
 
-	-- These are some examples, uncomment them if you want to see them work!
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("configs.lspconfig")
-		end,
-	},
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"vim",
-				"lua",
-				"vimdoc",
-				"html",
-				"css",
-				"javascript",
-				"typescript",
-				"markdown",
-				"markdown_inline",
-			},
-			indent = {
-				enable = true,
-			},
-		},
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "markdown",
+        "markdown_inline",
+      },
+      indent = {
+        enable = true,
+      },
+    },
+  },
 
-	{ "ludovicchabant/vim-gutentags", lazy = false },
+  { "ludovicchabant/vim-gutentags", lazy = false },
 
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				-- lua stuff
-				"lua-language-server",
-				"stylua",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
 
-				-- web dev stuff
-				"css-lsp",
-				"html-lsp",
-				"typescript-language-server",
-				"deno",
-				"prettier",
-				"eslint-lsp",
+        -- web dev stuff
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "deno",
+        "prettier",
+        "eslint-lsp",
 
-				-- shell
-				"shfmt",
+        -- shell
+        "shfmt",
 
-				-- sql
-				"sqlfmt",
-				"sqls",
+        -- sql
+        "sqlfmt",
+        "sqls",
 
-				-- c/cpp stuff
-				"clangd",
-				"clang-format",
-			},
-		},
-	},
+        -- c/cpp stuff
+        "clangd",
+        "clang-format",
+      },
+    },
+  },
 
-	{
-		"toppair/peek.nvim",
-		event = { "VeryLazy" },
-		build = "deno task --quiet build:fast",
-		config = function()
-			require("peek").setup()
-			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-		end,
-	},
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
 
-	{
-		"kevinhwang91/nvim-ufo",
-		lazy = false,
-		dependencies = {
-			{
-				"kevinhwang91/promise-async",
-			},
-		},
-		config = function()
-			vim.o.foldlevel = 99
-			require("ufo").setup({
-				provider_selector = function()
-					return { "treesitter", "indent" }
-				end,
-			})
-		end,
-	},
+  {
+    "kevinhwang91/nvim-ufo",
+    lazy = false,
+    dependencies = {
+      {
+        "kevinhwang91/promise-async",
+      },
+    },
+    config = function()
+      vim.o.foldlevel = 99
+      require("ufo").setup {
+        provider_selector = function()
+          return { "treesitter", "indent" }
+        end,
+      }
+    end,
+  },
 
-	{
-		"preservim/tagbar",
-		lazy = false,
-	},
+  {
+    "preservim/tagbar",
+    lazy = false,
+  },
 
-	{
-		"crusj/bookmarks.nvim",
-		keys = {
-			{ "<tab><tab>", mode = { "n" } },
-		},
-		branch = "main",
-		dependencies = { "nvim-web-devicons" },
-		config = function()
-			require("bookmarks").setup()
-			require("telescope").load_extension("bookmarks")
-		end,
-	},
+  {
+    "crusj/bookmarks.nvim",
+    keys = {
+      { "<tab><tab>", mode = { "n" } },
+    },
+    branch = "main",
+    dependencies = { "nvim-web-devicons" },
+    config = function()
+      require("bookmarks").setup()
+      require("telescope").load_extension "bookmarks"
+    end,
+  },
 }
